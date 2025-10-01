5d0ad08b3f659e82ee9d85a3b0f44a49c7b47354
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-10-02 00:08:03 +0200

    build: intialize native project with xcode

---
+++ NativeExample/NativeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeExampleApp.swift
+//  NativeExample
+//
+//  Created by Patryk Mleczek on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct NativeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
